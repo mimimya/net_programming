1c036db2c2ea7228b86295f97d4ca4cc65ff6fc2
mimimya <sj.ella1030@gmail.com>
2022-04-10 00:01:21 +0900

    hw7

---
+++ hw7/udp_loss_chat_server.py
@@ -0,0 +1,34 @@
+from socket import *
+import random
+
+port =3333
+BUFF_SIZE = 1024
+
+sock = socket(AF_INET, SOCK_DGRAM)
+sock.bind(('', port))
+
+while True:
+
+    while True:
+        sock.settimeout(None)
+        data, addr = sock.recvfrom(BUFF_SIZE)
+        if random.random() <= 0.5:#50%확률로 손실
+            continue
+        else:
+            sock.sendto(b'ack', addr)
+            print('<-', data.decode())
+            break
+
+    msg = input('->')
+    reTx = 0
+    while reTx <=3:
+        resp = str(reTx) + ' ' +msg
+        sock.sendto(resp.encode(), addr)
+        sock.settimeout(2)
+        try:
+            data, addr = sock.recvfrom(BUFF_SIZE)
+        except timeout:#ack을 시간 안에 못받았을 대
+            reTx += 1
+            continue
+        else:
+            break #시간 안에 받았을 때
